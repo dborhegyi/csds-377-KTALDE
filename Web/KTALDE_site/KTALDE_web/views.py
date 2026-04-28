563bda2ffba5a7e2cf45b89be04024815d4607f0
dborhegyi <44074532+dborhegyi@users.noreply.github.com>
2026-04-28 10:54:39 -0400

    fix

--- Web/KTALDE_site/KTALDE_web/views.py
+++ Web/KTALDE_site/KTALDE_web/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'lampi_web/index.html')
+    return render(request, 'KTALDE_web/index.html')
