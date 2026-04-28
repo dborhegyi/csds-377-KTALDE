e14cf7948d1957fe4982d243032a7413d04407e7
dborhegyi <44074532+dborhegyi@users.noreply.github.com>
2026-04-28 11:31:26 -0400

    login page dead

--- Web/KTALDE_site/KTALDE_web/views.py
+++ Web/KTALDE_site/KTALDE_web/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -10,31 +8,3 @@ def index(request):
         'lampi_list': []
     }
     return render(request, 'KTALDE_web/index.html', context)
-
-
-class CustomLoginView(LoginView):
-    template_name = 'KTALDE_web/login.html'
-    redirect_authenticated_user = True
-
-
-class CustomLogoutView(LogoutView):
-    next_page = '/'
-
-
-@login_required(login_url='login')
-def detail(request, device_id):
-    """View device details."""
-    context = {
-        'device_id': device_id,
-    }
-    return render(request, 'KTALDE_web/detail.html', context)
-
-
-@login_required(login_url='login')
-def add(request):
-    """Add a new LAMPI device."""
-    if request.method == 'POST':
-        # Handle form submission
-        return redirect('KTALDE_web:index')
-    
-    return render(request, 'KTALDE_web/addlampi.html')
