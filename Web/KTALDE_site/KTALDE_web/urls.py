381cfa9f8f1af488dcbedbd1806b94ea1145cc77
dborhegyi <44074532+dborhegyi@users.noreply.github.com>
2026-05-03 10:32:35 -0400

    pls work

--- Web/KTALDE_site/KTALDE_web/urls.py
+++ Web/KTALDE_site/KTALDE_web/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, re_path
 
 from . import views
-from my_app.views import MyPDFView
 
 app_name = 'KTALDE_web'
 
@@ -9,7 +8,7 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     re_path(r'^device/(?P<device_id>[0-9a-fA-F]+)$',
             views.DetailView.as_view(), name='detail'),
-    path('pdf-file/', MyPDFView.as_view(response_type='pdf'), name='pdf-file'),
-    path('pdf-html/', MyPDFView.as_view(response_type='html'), name='pdf-html'),
-    path('pdf-download/', MyPDFView.as_view(response_type='download'), name='pdf-download'),
+    path('pdf-file/', views.MyPDFView.as_view(response_type='pdf'), name='pdf-file'),
+    path('pdf-html/', views.MyPDFView.as_view(response_type='html'), name='pdf-html'),
+    path('pdf-download/', views.MyPDFView.as_view(response_type='download'), name='pdf-download'),
 ]
