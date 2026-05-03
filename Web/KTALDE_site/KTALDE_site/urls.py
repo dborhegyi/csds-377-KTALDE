7a1280c6a0e962030b2f1798ee3addf19724da12
dborhegyi <44074532+dborhegyi@users.noreply.github.com>
2026-05-03 12:13:26 -0400

    fix url

--- Web/KTALDE_site/KTALDE_site/urls.py
+++ Web/KTALDE_site/KTALDE_site/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('KTALDE_web.urls')),
     path('startgame/', views.StartGameView.as_view(), name='startgame'),
+    path('ingame/', views.view_pdf, name='ingame'),
 ]
