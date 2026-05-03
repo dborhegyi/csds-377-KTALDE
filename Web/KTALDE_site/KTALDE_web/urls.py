cb8ce1ba0a18808c07b79c79594d6f9375a865ae
dborhegyi <44074532+dborhegyi@users.noreply.github.com>
2026-05-03 12:15:07 -0400

    asfdcasdf

--- Web/KTALDE_site/KTALDE_web/urls.py
+++ Web/KTALDE_site/KTALDE_web/urls.py
@@ -6,8 +6,6 @@ app_name = 'KTALDE_web'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    re_path(r'^device/(?P<device_id>[0-9a-fA-F]+)$',
-            views.DetailView.as_view(), name='detail'),
     path('ingame/', views.view_pdf, name='ingame'),
 ]
 
