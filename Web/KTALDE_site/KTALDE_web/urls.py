40e8a02794c7b3dd6b79314ce90d606d0a283efa
dborhegyi <44074532+dborhegyi@users.noreply.github.com>
2026-05-06 03:01:16 -0400

    web url fix

--- Web/KTALDE_site/KTALDE_web/urls.py
+++ Web/KTALDE_site/KTALDE_web/urls.py
@@ -1,11 +1,8 @@
-from django.urls import path, re_path
-
+from django.urls import path
 from . import views
 
 app_name = 'KTALDE_web'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('ingame/', views.view_pdf, name='ingame'),
 ]
-
