5d4de1006768fc4835d9bd3cd98bd492b3308729
norahtn <norahtinsley@gmail.com>
2026-05-06 16:00:09 -0400

    various association changes

--- Web/KTALDE_site/KTALDE_web/models.py
+++ Web/KTALDE_site/KTALDE_web/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 import json
 import paho.mqtt.publish
 import secrets
