f274533b15f7f3b5c02cbc2d41c92ba7f02c0a47
dborhegyi <44074532+dborhegyi@users.noreply.github.com>
2026-05-06 03:04:21 -0400

    more url fix

--- Web/KTALDE_site/KTALDE_web/views.py
+++ Web/KTALDE_site/KTALDE_web/views.py
@@ -7,22 +7,7 @@ import os
 
 class StartGameView(generic.TemplateView):
     template_name = 'KTALDE_web/startgame.html'
-
-
-class IndexView(generic.TemplateView):
-    template_name = 'KTALDE_web/index.html'
-
-
-class DetailView(generic.TemplateView):
-    template_name = 'KTALDE_web/detail.html'
-
-    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
-        context = super(DetailView, self).get_context_data(**kwargs)
-        context['device'] = get_object_or_404(
-            Lampi, pk=kwargs['device_id'], user=self.request.user)
-        print("CONTEXT: {}".format(context))
-        return context
-    
+ 
 def view_pdf(request):
     try:
         # Open the PDF file in binary read mode
