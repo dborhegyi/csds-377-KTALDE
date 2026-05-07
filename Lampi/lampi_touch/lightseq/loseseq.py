a50493ad39aed0266a77ec5a476b4ac8fc40f7e9
norahtn <norahtinsley@gmail.com>
2026-05-06 23:54:27 -0400

    fixed self definitions again for real

--- Lampi/lampi_touch/lightseq/loseseq.py
+++ Lampi/lampi_touch/lightseq/loseseq.py
@@ -3,13 +3,13 @@ import time
 
 class LosingSequence:
     def __init__(self):
+        self.blue = 0
+        self.red = 0
+        self.green = 0
         self.pi1 = pigpio.pi()
-        blue = self.blue
-        red = self.red
-        green = self.green
-        self.pi1.write(blue, 0)
-        self.pi1.write(red, 0)
-        self.pi1.write(green,0)
+        self.pi1.write(self.blue, 0)
+        self.pi1.write(self.red, 0)
+        self.pi1.write(self.green,0)
 
     def run(self):
 
@@ -24,7 +24,7 @@ class LosingSequence:
                 # Delay 1 second
                 time.sleep(0.25)
     
-                self.pi1.write(red,1)
+                self.pi1.write(self.red,1)
                 time.sleep(0.5)
 
         except KeyboardInterrupt: # ctrl C exit
